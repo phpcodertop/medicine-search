362ac0cf05f0dea79d7245e12f07a59901e5320b
Ahmed Halima <phpcodertop@gmail.com>
2020-12-11 01:50:15 +0200

    remove datatables and adjust main route

--- app/Http/Controllers/MedicinesController.php
+++ app/Http/Controllers/MedicinesController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Medicine;
-use Illuminate\Http\Request;
-use Yajra\DataTables\Facades\DataTables;
 
 class MedicinesController extends Controller
 {
